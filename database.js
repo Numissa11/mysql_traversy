4d06d34dbd2a66251128ec771922b7a8bbde9c96
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-12-08 18:48:42 +0100

    db queries get/post

--- database.js
+++ database.js
@@ -1,12 +1,37 @@
 import mysql from 'mysql2';
 
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
     host:'localhost',
     user:'sqluser',
     password: 'password',
     database: 'notes_app'
-});
+}).promise()
 
-const result = pool.query('SELECT * FROM notes', (err, res) => {
-    console.log('',res);
-})
+
+async function getNotes() 
+{
+    const [result] = await pool.query('SELECT * FROM notes');
+    return result
+}
+
+async function getNote(id) 
+{
+    const [result] = await pool.query(`
+    SELECT * FROM notes
+    WHERE id = ${id}
+    `);
+    return result[0]
+}
+
+async function createNote(title, contents){
+    const myTitle= title
+    const cont= contents
+    const [result] = await pool.query(`INSERT INTO notes(title, contents) VALUES('${myTitle}','${cont}')`)
+    return result
+}
+
+const creation = await createNote('myBAD', 'Youpiiiiiiii');
+const notes = await getNotes();
+const note = await getNote(1);
+console.log('note', notes);
+console.log('creation',creation.insertId);
